f796bef63900a262b75426be525e7d273c356236
MmanawaMarongwe <140721349+MmanawaMarongwe@users.noreply.github.com>
2025-07-22 18:53:31 +0200

    Added the user prompts and converts them to lowercase

---
+++ script.js
@@ -0,0 +1,11 @@
+let taskTitle1 = prompt("Enter task 1 title");
+let taskDescription1 = prompt("Enter task 1 description");
+let taskStatus1 = prompt("Enter task status");
+
+taskDescription1.toLowerCase;
+
+let taskTitle2 = prompt("Enter task 1 title");
+let taskDescription2 = prompt("Enter task 1 description");
+let taskStatus2 = prompt("Enter task status");
+
+taskDescription2.toLowerCase;
