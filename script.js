aa03bb0c6845e3b6e877fdd9bcc1c75233ad1da0
MmanawaMarongwe <140721349+MmanawaMarongwe@users.noreply.github.com>
2025-07-22 18:59:49 +0200

    Corrected toLowercase by adding parenthesis

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ let taskTitle1 = prompt("Enter task 1 title");
 let taskDescription1 = prompt("Enter task 1 description");
 let taskStatus1 = prompt("Enter task status");
 
-taskDescription1.toLowerCase;
+console.log(taskDescription1.toLowerCase());
 
 let taskTitle2 = prompt("Enter task 1 title");
 let taskDescription2 = prompt("Enter task 1 description");
 let taskStatus2 = prompt("Enter task status");
 
-taskDescription2.toLowerCase;
+console.log(taskDescription2.toLowerCase());
