78ad66078a7bbeea3bb4e4110ad82b91bdd31af0
MmanawaMarongwe <140721349+MmanawaMarongwe@users.noreply.github.com>
2025-07-22 22:22:02 +0200

    log done tasks with quotes and show message only if no task is completed

--- script.js
+++ script.js
@@ -12,16 +12,17 @@ while (
   alert(
     "Invalid status. Please enter one of the following: todo, doing, or done."
   );
-  taskStatus1 = prompt("Enter task status").toLowerCase();
+  taskStatus1 = prompt("Enter task 1 status").toLowerCase();
 }
 
-// If task is not marked as "todo", log its title and status; otherwise, encourage the user to get started
-if (taskStatus1 !== "todo") {
-  console.log(`Title: ${taskTitle1}, status: ${taskStatus1}`);
+// If task is marked as "done", log its title and status; otherwise, encourage the user to get started or keep going
+if (taskStatus1 === "done") {
+  console.log(`Title:"${taskTitle1}", Status: "${taskStatus1}"`);
 } else {
   console.log("No tasks completed, let's get to work!");
 }
 
+// Prompt for Task 2
 let taskTitle2 = prompt("Enter task 2 title");
 let taskDescription2 = prompt("Enter task 2 description");
 let taskStatus2 = prompt("Enter task 2 status").toLowerCase();
@@ -38,9 +39,9 @@ while (
   taskStatus2 = prompt("Enter task status").toLowerCase();
 }
 
-// If task is not marked as "todo", log its title and status; otherwise, encourage the user to get started
-if (taskStatus2 !== "todo") {
-  console.log(`Title: ${taskTitle2}, status: ${taskStatus2}`);
+// If task is marked as "done", log its title and status; otherwise, encourage the user to get started or keep going
+if (taskStatus2 === "done") {
+  console.log(`Title: "${taskTitle2}", status: "${taskStatus2}"`);
 } else {
   console.log("No tasks completed, let's get to work!");
 }
